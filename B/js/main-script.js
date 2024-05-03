22503ddc5d68ac1d2c059a2fd514d37d37586888
Simão Sanguinho <simao.sanguinho@tecnico.ulisboa.pt>
2024-05-03 09:24:49 +0100

    refactor: transfer all code to main-script.js

--- B/js/main-script.js
+++ B/js/main-script.js
@@ -5,30 +5,159 @@ import * as Stats from "three/addons/libs/stats.module.js";
 import { GUI } from "three/addons/libs/lil-gui.module.min.js";
 
 // import constants.js
-import {
-  UNIT,
-  Primitives,
-  AXIS,
-  cameraValues,
-  CLOCK,
-  DELTA_MULT,
-  backgroundColor,
-  fov,
-  minViewDistance,
-  maxViewDistance,
-} from "./constants.js";
-import { createCrane } from "./crane.js";
-import { getMeshesToUpdate } from "./utils.js";
+//////////////////////
+/* CONSTANTS        */
+//////////////////////
+const UNIT = 20;
 
-let camera, scene, renderer, delta, axes;
-let isAnimating;
+const CLOCK = new THREE.Clock();
+
+const DELTA_MULT = 100;
+
+const backgroundColor = 0xeaf6ff;
+
+const fov = 70;
+
+const minViewDistance = 1;
+
+const maxViewDistance = 10000;
+
+const AXIS = {
+  X: "x",
+  Y: "y",
+  Z: "z",
+};
+
+const Primitives = {
+  CUBE: "cube",
+  CYLINDER: "cylinder",
+  TETAEDRON: "tetraedron",
+};
+
+const cameraValues = [
+  [0, 0, 1000],
+  [1000, 0, 0],
+  [0, 1000, 0],
+  [2000, 1000, 3000],
+  [1000, 1000, 1000],
+  [500, 2000, 2000],
+];
+
+// REMOVE
+const colors = {
+  white: 0xffffff,
+  black: 0x000000,
+  red: 0xff0000,
+  green: 0x00ff00,
+  blue: 0x0000ff,
+  yellow: 0xffff00,
+  cyan: 0x00ffff,
+  magenta: 0xff00ff,
+};
+
+const cranePosition = {
+  positionX: 4.5 * UNIT,
+  positionY: 0 * UNIT,
+  positionZ: 1.5 * UNIT,
+};
+
+const baseVals = {
+  width: 3 * UNIT,
+  depth: 3 * UNIT,
+  height: 1 * UNIT,
+  positionX: 0 * UNIT,
+  positionY: 0.5 * UNIT,
+  positionZ: 0 * UNIT,
+  type: Primitives.CUBE,
+  material: new THREE.MeshBasicMaterial({ color: colors.red }), //transparent: true, opacity: 0.5,
+};
+
+const towerVals = {
+  width: 1 * UNIT,
+  depth: 1 * UNIT,
+  height: 12 * UNIT,
+  positionX: 0 * UNIT,
+  positionY: 7 * UNIT,
+  positionZ: 0 * UNIT,
+  type: Primitives.CUBE,
+  material: new THREE.MeshBasicMaterial({ color: colors.blue }),
+};
+
+const cabVals = {
+  width: 2 * UNIT,
+  depth: 2 * UNIT,
+  height: 2 * UNIT,
+  positionX: 0 * UNIT,
+  positionY: 14 * UNIT,
+  positionZ: 0 * UNIT,
+  type: Primitives.CUBE,
+  material: new THREE.MeshBasicMaterial({ color: colors.yellow }),
+};
+
+const jibVals = {
+  width: 18 * UNIT,
+  depth: 1 * UNIT,
+  height: 1 * UNIT,
+  positionX: 4.5 * UNIT,
+  positionY: 15.5 * UNIT,
+  positionZ: 0 * UNIT,
+  type: Primitives.CUBE,
+  material: new THREE.MeshBasicMaterial({ color: colors.green }),
+};
+
+const upperTowerVals = {
+  width: 1 * UNIT,
+  depth: 1 * UNIT,
+  height: 3 * UNIT,
+  positionX: 0 * UNIT,
+  positionY: 17.5 * UNIT,
+  positionZ: 0 * UNIT,
+  type: Primitives.CUBE,
+  material: new THREE.MeshBasicMaterial({ color: colors.blue }),
+};
+
+const counterWeightVals = {
+  width: 1.5 * UNIT,
+  depth: 1 * UNIT,
+  height: 0.5 * UNIT,
+  positionX: -3.25 * UNIT,
+  positionY: 14.75 * UNIT,
+  positionZ: 0 * UNIT,
+  type: Primitives.CUBE,
+  material: new THREE.MeshBasicMaterial({ color: colors.red }),
+};
+
+const trolleyVals = {
+  width: 2 * UNIT,
+  depth: 1 * UNIT,
+  height: 0.3 * UNIT,
+  positionX: 11.5 * UNIT,
+  positionY: 14.85 * UNIT,
+  positionZ: 0 * UNIT,
+  type: Primitives.CUBE,
+  material: new THREE.MeshBasicMaterial({ color: colors.red }),
+};
+
+const cableVals = {
+  width: 0.2 * UNIT,
+  depth: 0.2 * UNIT,
+  height: 5.7 * UNIT,
+  positionX: 11.5 * UNIT,
+  positionY: 12 * UNIT,
+  positionZ: 0 * UNIT,
+  type: Primitives.CUBE,
+  material: new THREE.MeshBasicMaterial({ color: colors.green }),
+};
 
 //////////////////////
 /* GLOBAL VARIABLES */
 //////////////////////
 const cameras = [];
-
+let meshesToUpdate = [];
+let lowerStructure;
 let currentCamera;
+let camera, scene, renderer, delta, axes;
+let isAnimating;
 
 /////////////////////
 /* CREATE SCENE(S) */
@@ -99,6 +228,196 @@ function createOrtographicCamera(cameraValue) {
 ////////////////////////
 /* CREATE OBJECT3D(S) */
 ////////////////////////
+
+function createObject(objectVals) {
+  "use strict";
+
+  const object = new THREE.Object3D();
+
+  let geometry;
+
+  switch (objectVals.type) {
+    case Primitives.CUBE:
+      console.log("Cube");
+      geometry = new THREE.BoxGeometry(
+        objectVals.width,
+        objectVals.height,
+        objectVals.depth
+      );
+      break;
+    case Primitives.CYLINDER:
+      console.log("Cylinder");
+      geometry = new THREE.CylinderGeometry(
+        objectVals.radiusTop,
+        objectVals.radiusBottom,
+        objectVals.height
+      );
+      break;
+    case Primitives.TETRAEDRON:
+      console.log("Tetrahedron");
+      geometry = new THREE.TetrahedronGeometry(objectVals.radius);
+      break;
+    default:
+      console.log("Object type not found");
+      break;
+  }
+  // TODO: ADD MATERIAL
+  const mesh = new THREE.Mesh(geometry, objectVals.material);
+  object.add(mesh);
+
+  /* let edges = new THREE.EdgesGeometry(mesh.geometry);
+    let line = new THREE.LineSegments(edges, new THREE.LineBasicMaterial({ color: 0xffffff }));
+    // increase the size of the edges
+    line.scale.set(1.1, 1.1, 1.1);
+  
+    object.add(line); */
+
+  meshesToUpdate.push(mesh);
+  return object;
+}
+
+function setPosition(object, vals) {
+  "use strict";
+  object.position.set(vals.positionX, vals.positionY, vals.positionZ);
+}
+
+function rotateObject(object, rotationVals, axis) {
+  "use strict";
+
+  switch (axis) {
+    case AXIS.X:
+      object.rotation.x = THREE.Math.clamp(
+        object.userData.step * delta + object.rotation.x,
+        rotationVals.min,
+        rotationVals.max
+      );
+      break;
+
+    case AXIS.Y:
+      object.rotation.y += THREE.Math.clamp(
+        object.userData.step * delta + object.rotation.y,
+        rotationVals.min,
+        rotationVals.max
+      );
+      break;
+
+    case AXIS.Z:
+      object.rotation.z += THREE.Math.clamp(
+        object.userData.step * delta + object.rotation.z,
+        rotationVals.min,
+        rotationVals.max
+      );
+      break;
+
+    default:
+      console.log("Axis not found");
+  }
+}
+
+function getMeshesToUpdate() {
+  return meshesToUpdate;
+}
+
+function createCrane() {
+  "use strict";
+
+  let crane = new THREE.Group();
+  let lowerStructure = createLowerStructure();
+  let upperStructure = createUpperStructure();
+
+  crane.add(lowerStructure);
+  crane.add(upperStructure);
+
+  setPosition(crane, cranePosition);
+  scene.add(crane);
+}
+
+function createLowerStructure() {
+  "use strict";
+  let group = new THREE.Group();
+  const base = createBase();
+  const tower = createTower();
+
+  group.add(base);
+  group.add(tower);
+  return group;
+}
+
+function createUpperStructure() {
+  "use strict";
+  // TODO: In order to rotate the object 'group' must be the one declared in main-script.js
+  let group = new THREE.Group();
+  const cab = createCab();
+  const jib = createJib();
+  const upperTower = createUpperTower();
+  const counterWeight = createCounterWeight();
+  const trolley = createTrolley();
+  const cable = createCable();
+
+  group.add(cab);
+  group.add(jib);
+  group.add(upperTower);
+  group.add(counterWeight);
+  group.add(trolley);
+  group.add(cable);
+  return group;
+}
+
+function createBase() {
+  "use strict";
+  const base = createObject(baseVals);
+  setPosition(base, baseVals);
+  return base;
+}
+
+function createTower() {
+  "use strict";
+  const tower = createObject(towerVals);
+  setPosition(tower, towerVals);
+  return tower;
+}
+
+function createCab() {
+  "use strict";
+  const cab = createObject(cabVals);
+  setPosition(cab, cabVals);
+  return cab;
+}
+
+function createJib() {
+  "use strict";
+  const jib = createObject(jibVals);
+  setPosition(jib, jibVals);
+  return jib;
+}
+
+function createUpperTower() {
+  "use strict";
+  const upperTower = createObject(upperTowerVals);
+  setPosition(upperTower, upperTowerVals);
+  return upperTower;
+}
+
+function createCounterWeight() {
+  "use strict";
+  const counterWeight = createObject(counterWeightVals);
+  setPosition(counterWeight, counterWeightVals);
+  return counterWeight;
+}
+
+function createTrolley() {
+  "use strict";
+  const trolley = createObject(trolleyVals);
+  setPosition(trolley, trolleyVals);
+  return trolley;
+}
+
+function createCable() {
+  "use strict";
+  const cable = createObject(cableVals);
+  setPosition(cable, cableVals);
+  return cable;
+}
 
 //////////////////////
 /* CHECK COLLISIONS */
@@ -332,10 +651,8 @@ function onKeyUp(e) {
 init();
 animate();
 
-export { scene, renderer, currentCamera };
-
 ///////////////////////
-/* Heads-Up Display */
+/* Heads-Up Display  */
 ///////////////////////
 
 function makeButtonActive(key) {
@@ -354,14 +671,14 @@ function makeButtonInactive(key) {
   }
 }
 
-///////////////////////
-/* Wireframe */
+////////////////////////
+/*    WIREFRAME       */
 ///////////////////////
 
 let isWireframe = false; // Variable to track wireframe mode
 
 function updateWireframe() {
-    getMeshesToUpdate().forEach(mesh => {
-        mesh.material.wireframe = isWireframe;
-    });
+  getMeshesToUpdate().forEach((mesh) => {
+    mesh.material.wireframe = isWireframe;
+  });
 }
